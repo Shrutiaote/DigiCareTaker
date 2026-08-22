4112f0366cf54ff57e33df113cfb27bc58accb8f
Shruti Aote <shrutiaote@gmail.com>
2026-08-22 18:35:09 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/digicaretaker/DigicaretakerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digicaretaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigicaretakerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigicaretakerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digicaretaker/DigicaretakerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digicaretaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigicaretakerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
